b36d48f4f4738cf4eec99f4eea65f95ca15ebedb
LMGJordao <lmgjordao@gmail.com>
2022-08-15 18:37:41 +0100

    part2a
    courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,30 @@
+const Part = ({part}) => <p key={part.id}>{part.name} {part.exercises}</p>;
+
+const Total = ({parts}) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return (
+        <h3>total of {total} exercises</h3>
+    );
+};
+
+const Header = ({name}) => <h1>{name}</h1>;
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => <Part part={part} />)}
+            <Total parts={parts} />
+        </div>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+        </div>
+    )
+};
+
+export default Course;
